2e2bd1ac7f46a63272cbcb478f003ba528b663eb
Anonymous299 <62924701+Anonymous299@users.noreply.github.com>
2021-01-25 05:26:37 +0530

    Testing stock price plotting

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 import datetime as dt
 import pandas as pd
 import pandas_datareader.data as web
+import matplotlib.pyplot as plt
+from matplotlib import style
 
+##start = dt.datetime(2000,1,1)
+##end = dt.datetime(2020, 12, 31)
+##
+##df = web.DataReader('TSLA', 'yahoo', start, end)
+##df.to_csv('tsla.csv')
 
-start = dt.datetime(2000,1,1)
-end = dt.datetime(2020, 12, 31)
+style.use('ggplot')
 
-df = web.DataReader('TSLA', 'yahoo', start, end)
-df.to_csv('tsla.csv')
+df = pd.read_csv('tsla.csv', index_col=0, parse_dates=True)
+
+df['Adj Close'].plot()
+plt.show()
