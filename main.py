40d62b98f03cdf337545ff68a1f3ed23b8ee2f4e
Anonymous299 <62924701+Anonymous299@users.noreply.github.com>
2021-01-25 05:50:35 +0530

    Added column for 100 MA

--- main.py
+++ main.py
@@ -11,8 +11,16 @@ from matplotlib import style
 ##df.to_csv('tsla.csv')
 
 style.use('ggplot')
-
 df = pd.read_csv('tsla.csv', index_col=0, parse_dates=True)
 
-df['Adj Close'].plot()
+df['100ma'] = df['Adj Close'].rolling(window=100, min_periods=0).mean()
+print(df.head())
+
+ax1 = plt.subplot2grid((6, 1), (0, 0), rowspan=5, colspan=1)
+ax2 = plt.subplot2grid((6, 1), (5, 0), rowspan=1, colspan=1, sharex = ax1)
+
+ax1.plot(df.index, df['Adj Close'])
+ax1.plot(df.index, df['100ma'])
+ax2.bar(df.index, df['Volume'])
+##df['Adj Close'].plot()
 plt.show()
