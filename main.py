5d2b144630dcd177ac79d59771fe95bb0808be70
Anonymous299 <62924701+Anonymous299@users.noreply.github.com>
2021-01-25 17:29:33 +0530

    Built candlestick chart

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import datetime as dt
 import pandas as pd
 import pandas_datareader.data as web
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 from matplotlib import style
+from mplfinance.original_flavor import candlestick_ohlc
 
 ##start = dt.datetime(2000,1,1)
 ##end = dt.datetime(2020, 12, 31)
@@ -13,14 +15,17 @@ from matplotlib import style
 style.use('ggplot')
 df = pd.read_csv('tsla.csv', index_col=0, parse_dates=True)
 
-df['100ma'] = df['Adj Close'].rolling(window=100, min_periods=0).mean()
-print(df.head())
+df_ohlc = df['Adj Close'].resample('10D').ohlc()
+df_volume = df['Volume'].resample('10D').sum()
+
+df_ohlc.reset_index(inplace=True)
+df_ohlc['Date'] = df_ohlc['Date'].map(mdates.date2num)
 
 ax1 = plt.subplot2grid((6, 1), (0, 0), rowspan=5, colspan=1)
 ax2 = plt.subplot2grid((6, 1), (5, 0), rowspan=1, colspan=1, sharex = ax1)
+ax1.xaxis_date()
 
-ax1.plot(df.index, df['Adj Close'])
-ax1.plot(df.index, df['100ma'])
-ax2.bar(df.index, df['Volume'])
-##df['Adj Close'].plot()
+candlestick_ohlc(ax1, df_ohlc.values, width=2, colorup='g', colordown='r')
+ax2.fill_between(df_volume.index.map(mdates.date2num), df_volume.values, 0)
+
 plt.show()
