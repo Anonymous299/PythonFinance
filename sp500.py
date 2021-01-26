1899d765ad35eacc1ab9e0637f87fd25b2056996
Anonymous299 <62924701+Anonymous299@users.noreply.github.com>
2021-01-26 07:15:34 +0530

    Created function for getting S&P 500 companies

---
+++ sp500.py
@@ -0,0 +1,20 @@
+import pickle
+import bs4 as bs
+import requests
+
+def save_sp500_tickers():
+    resp = requests.get('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
+    soup = bs.BeautifulSoup(resp.text, "lxml")
+    table = soup.find('table', {'class': 'wikitable sortable'})
+    tickers = []
+    for row in table.findAll('tr')[1:]:
+        ticker = row.findAll('td')[0].text[:-1]
+        tickers.append(ticker)
+
+    with open("sp500tickers.pickle", "wb") as f:
+        pickle.dump(tickers, f)
+
+    print(tickers)
+
+    return tickers
+
