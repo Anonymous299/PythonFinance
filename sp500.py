aeec82fc92266221e6544ebaf27b7796ebe1cfa6
Anonymous299 <sahilpradhan299@gmail.com>
2021-01-28 23:23:50 -0500

    Add comments

--- sp500.py
+++ sp500.py
@@ -1,3 +1,5 @@
+## This program is used to gather s&p500 stock data and calculates correlation between them
+
 import pickle
 import bs4 as bs
 import requests
@@ -11,8 +13,10 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from matplotlib import style
 
-style.use('ggplot')
+style.use('ggplot') # Sets style for matplotlib
 
+# Collects, stores and returns names of all s&p500 company tickers
+# effects: produces output
 def save_sp500_tickers():
     resp = requests.get('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
     soup = bs.BeautifulSoup(resp.text, "lxml")
@@ -29,6 +33,10 @@ def save_sp500_tickers():
 
     return tickers
 
+# collects and stores data for all collected s&p500 tickers from yahoo
+# reload_sp500_ticker - Set to True to reload names of s&p500 tickers
+# reload_sp500_data - Set to True to reload s&p500 data
+# effects: produces output
 def get_data_from_yahoo(reload_sp500_ticker=False, reload_sp500_data=False):
     if reload_sp500_ticker:
         tickers = save_sp500_tickers()
@@ -56,6 +64,7 @@ def get_data_from_yahoo(reload_sp500_ticker=False, reload_sp500_data=False):
     else:
         print('Already have data')
             
+# creates and stores table of s&p 500 Adj close data joined together in one table            
 def create_joined_table():
     if not os.path.exists('stock_dfs/sp500data.pickle'):
         print('Data does not exist')
@@ -81,6 +90,7 @@ def create_joined_table():
     # for key in data['MSFT'].keys():
     #     print(key)
 
+# Calculates correlation between Adj close prices and creates a corresponding heatmap.
 def visualize_data():
     if not os.path.exists('stock_dfs/sp500close.csv'):
         print('Data does not exist')
@@ -90,13 +100,17 @@ def visualize_data():
     main_dfs.set_index('Date', inplace=True) 
     # corr_dfs = main_dfs.pct_change().corr()
     corr_dfs = main_dfs.corr()
+    filter_negative_correlation(corr_dfs)
     # corr_dfs.to_csv('analysis/price_correlation/price_corr.csv')
-
-    mean_corr = corr_dfs.mean()
-    print(mean_corr.where(mean_corr.values < 0).dropna())
 
     sns.heatmap(corr_dfs,cmap='RdYlGn', xticklabels=True, yticklabels=True)
     plt.show()
+    
+
+# Takes in a correlation table and filters out the negatively correlated stocks
+def filter_negative_correlation(corr_dfs):
+    mean_corr = corr_dfs.mean()
+    print(mean_corr.where(mean_corr.values < 0).dropna())
 
 # get_data_from_yahoo()
 # create_joined_table()
