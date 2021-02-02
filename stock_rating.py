cc18102074cb606c1067394328b702103836acb7
Anonymous299 <sahilpradhan299@gmail.com>
2021-02-02 01:01:20 -0500

    Add feature and label extraction function

--- stock_rating.py
+++ stock_rating.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import numpy as np
+from collections import Counter
 
 # Preprocess stock data for machine learning algorithm
 # Creates column which calculates pct change in stock upto 7 days
@@ -12,8 +14,8 @@ def preprocess_stock_data(ticker):
 	for i in range(1, period + 1):
 		main_dfs['{}_{}d'.format(ticker, i)] = (main_dfs[ticker].shift(-i) - main_dfs[ticker]) / main_dfs[ticker]
 
-	print(main_dfs)
-	return tickers, main_dfs
+	# print(main_dfs.head(15))
+	return tickers, main_dfs, period
 
 # Proxy function to classify stock as buy, sell or hold according 
 # to pct change in the next 7 days
@@ -27,3 +29,29 @@ def buy_sell_hold(*args):
 		if col < -req:
 			return -1
 	return 0
+
+# Build buy, sell and hold feature column for ticker
+def extract_featureset(ticker):
+	tickers, df, period = preprocess_stock_data(ticker)
+	df['{}_rating'.format(ticker)] = list(map(buy_sell_hold, *[df['{}_{}d'.format(ticker, i)] for i in range(1, period+1)]))
+	
+	# Calculating spread of ratings
+	vals = df['{}_rating'.format(ticker)].values.tolist()
+	str_vals = [str(i) for i in vals]
+	print('Rating spread:', Counter(str_vals))
+	df.fillna(0, inplace=True)
+
+	df = df.replace([np.inf, -np.inf], np.nan)
+	df.dropna(inplace=True)
+
+	df_vals = df[[ticker for ticker in tickers]].pct_change()
+	df_vals = df_vals.replace([np.inf, -np.inf], 0)
+	df_vals.fillna(0, inplace=True)
+
+	X = df_vals.values
+	y = df['{}_rating'.format(ticker)].values
+
+	return X, y, df
+
+# preprocess_stock_data('AAPL')
+extract_featureset('AAPL')
